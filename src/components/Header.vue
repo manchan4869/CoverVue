b5489d6694abf232d5c1c32318d7c55c54d8c4b0
成满 <2445937718@qq.com>
2023-01-28 20:05:48 +0800

    拆分 汉化

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="text-xl bg-white flex border-b border-gray-100 p-2">
+    <a class="flex items-center" href="/">
+      <!-- <img src="/static/media/logo.5d3dd1b68da4b08fcf92.png" alt="logo" class="w-8 h-8 mx-4"> -->
+      <h1 class="font-semibold">Coverview</h1>
+    </a>
+    <div class="ml-auto md:mr-4 ">
+      <a class="text-gray-700 hover:text-gray-800 text-base font-Nunito mx-4" href="/faq">
+        <span class="hidden md:inline-block">How to use</span>
+      </a>
+      <a href="https://www.buymeacoffee.com/rutikwankhade" target="_blank" rel="noreferrer"
+        class="text-base mx-2 font-Nunito">🥤
+        <span class="hidden md:inline-block">Buy me a coffee</span>
+      </a>
+      <a href="https://twitter.com/intent/tweet?text=type%20your%20thoughts%20here%2C%20Try%20https%3A%2F%2Fcoverview.vercel.app%20by%20%40WankhadeRutik"
+        class="mx-2 bg-blue-400 md:text-sm text-xs rounded-full px-4 font-semibold text-white p-1">Share on
+        Twitter</a>
+    </div>
+  </div>
+</template>
+
+
