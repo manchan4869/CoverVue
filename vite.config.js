2cd0e108464d5c31f8a355e2e7898ab86f5cdc36
成满 <2445937718@qq.com>
2023-02-05 00:24:42 +0800

    优化打包

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
+import { splitVendorChunkPlugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), splitVendorChunkPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: assetInfo => {
+          const info = assetInfo.name.split('.')
+          let extType = info[info.length - 1]
+          if (
+            /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name)
+          ) {
+            extType = 'media'
+          } else if (/\.(png|jpe?g|gif|svg)(\?.*)?$/.test(assetInfo.name)) {
+            extType = 'img'
+          } else if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name)) {
+            extType = 'fonts'
+            return `assets/${extType}/[hash][extname]`
+          }
+          return `assets/${extType}/[name]-[hash][extname]`
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js'
+      }
+    }
+  }
 })
